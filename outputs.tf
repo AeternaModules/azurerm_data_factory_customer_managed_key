52357379c62442b4b25c31fa072126f00bc2ef9c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:44 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "data_factory_customer_managed_keys" {
-  description = "All data_factory_customer_managed_key resources"
-  value       = azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys
-}
 output "data_factory_customer_managed_keys_customer_managed_key_id" {
-  description = "List of customer_managed_key_id values across all data_factory_customer_managed_keys"
-  value       = [for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : v.customer_managed_key_id]
+  description = "Map of customer_managed_key_id values across all data_factory_customer_managed_keys, keyed the same as var.data_factory_customer_managed_keys"
+  value       = { for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : k => v.customer_managed_key_id }
 }
 output "data_factory_customer_managed_keys_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_customer_managed_keys"
-  value       = [for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_customer_managed_keys, keyed the same as var.data_factory_customer_managed_keys"
+  value       = { for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : k => v.data_factory_id }
 }
 output "data_factory_customer_managed_keys_user_assigned_identity_id" {
-  description = "List of user_assigned_identity_id values across all data_factory_customer_managed_keys"
-  value       = [for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : v.user_assigned_identity_id]
+  description = "Map of user_assigned_identity_id values across all data_factory_customer_managed_keys, keyed the same as var.data_factory_customer_managed_keys"
+  value       = { for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : k => v.user_assigned_identity_id }
 }
 
