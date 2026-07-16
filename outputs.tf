329e06a624db33c3518a32107b78e0e1ea8746e7
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "data_factory_customer_managed_keys_id" {
   description = "Map of id values across all data_factory_customer_managed_keys, keyed the same as var.data_factory_customer_managed_keys"
-  value       = { for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : k => v.id }
+  value       = { for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "data_factory_customer_managed_keys_customer_managed_key_id" {
   description = "Map of customer_managed_key_id values across all data_factory_customer_managed_keys, keyed the same as var.data_factory_customer_managed_keys"
-  value       = { for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : k => v.customer_managed_key_id }
+  value       = { for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : k => v.customer_managed_key_id if v.customer_managed_key_id != null && length(v.customer_managed_key_id) > 0 }
 }
 output "data_factory_customer_managed_keys_data_factory_id" {
   description = "Map of data_factory_id values across all data_factory_customer_managed_keys, keyed the same as var.data_factory_customer_managed_keys"
-  value       = { for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : k => v.data_factory_id }
+  value       = { for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : k => v.data_factory_id if v.data_factory_id != null && length(v.data_factory_id) > 0 }
 }
 output "data_factory_customer_managed_keys_user_assigned_identity_id" {
   description = "Map of user_assigned_identity_id values across all data_factory_customer_managed_keys, keyed the same as var.data_factory_customer_managed_keys"
-  value       = { for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : k => v.user_assigned_identity_id }
+  value       = { for k, v in azurerm_data_factory_customer_managed_key.data_factory_customer_managed_keys : k => v.user_assigned_identity_id if v.user_assigned_identity_id != null && length(v.user_assigned_identity_id) > 0 }
 }
 
